093c689de44eef10e89a1fc9172951e2c3362e87
nargesbyt <nargesbyt123@gmail.com>
2024-12-12 09:37:10 +0330

    add sql for create table interview

--- public/database/2create_table_interviews.sql
+++ public/database/2create_table_interviews.sql
@@ -1,22 +1,25 @@
 CREATE TABLE IF NOT EXISTS `interviews` (
     id int NOT NULL auto_increment,
     firstname varchar(255),
-    lastname varchar(255),
-    interviewDate date,
+    lastname varchar(255)
+    interview_date date,
     education varchar(255),
     age int,
-    address varchar(255),
-    maritalStatus varchar(255),
-    childNum int,
-    phoneNum varchar(255),
-    careerFieldId int,
+    address_residence varchar(255),
+    marital_status varchar(255),
+    child_num int,
+    phone_num varchar(255),
+    career_field_id int,
     gender int,
-    employmentHistory int,
-    fatherJob varchar(255),
-    additionalDetails text,
-    interviewResult varchar(255),
-    resumeFile varchar(255),
+    employment_history_id int,
+    father_job varchar(255),
+    additional_details text,
+    interview_result varchar(255),
+    resume_file varchar(255),
+    user_id int,
     PRIMARY KEY  (`id`),
-    FOREIGN KEY (careerFieldId) REFERENCES careerfields(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (career_field_id) REFERENCES careerfields(id),
+    FOREIGN KEY (employment_history_id) REFERENCES employmenthistory(id)
     );
     
